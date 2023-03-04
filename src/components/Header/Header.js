695efda92d2b4065db329a8bffa83a4478ba3700
Kejvin Pashaj <kevin.952011@gmail.com>
2023-03-04 15:59:15 +0000

    modified navbar component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,28 @@
 import React from "react";
-import "./header.css";
+import "./Header.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
 
 function Header() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="*">Kev//Dev</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="*">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="*">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link" href="*" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                        </li>
-                    </ul>
-                    <a href="*" type="button" class="btn btn-outline-success">Resume</a>
-                </div>
-            </div>
-        </nav>
-    )
-}
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Kev//Dev</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+              <Button variant="outline-success">Resume</Button>{' '}
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    }
 
 export default Header;
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HomePage.css";
+
+function Home(){
+    
+}
+
+export default Home;
