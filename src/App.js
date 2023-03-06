6348f1c4952ea70ac8aab9fddb37220dfdc848ca
Kejvin Pashaj <kevin.952011@gmail.com>
2023-03-06 20:03:00 +0000

    fixed project cards layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Header from './components/Header/Header';
 import Home from './components/Home/HomePage';
 import Projects from './components/Projects/Projects';
 
 function App() {
+  const styles = {
+    textAlign: "center"
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={styles}>
       <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,46 +14,22 @@ function Projects() {
                 <h1 className="projects-title">Projects</h1>
                 <div className="projects-container">
                     <Row xs={1} md={1} lg={3} className="g-4">
-                        <Col>
-                            {projectData.map(data => <Project
-                                name={data.name}
-                                info={data.info}
-                                image={data.image}
-                                github={data.github}
-                                deployed={data.deployed}
-                            />)}
-                        </Col>
+                        {projectData.map((data) => (
+                            <Col>
+                                <Project
+                                    name={data.name}
+                                    info={data.info}
+                                    image={data.image}
+                                    github={data.github}
+                                    deployed={data.deployed}
+                                />
+                            </Col>
+                        ))}
                     </Row>
                 </div>
             </Container>
         </div>
-    )
+    );
 }
-
-
-// function Projects() {
-//     return (
-//         <div className="projects">
-//             <Container>
-//                 <h1 className="projects-title">Projects</h1>
-//                 <div className="projects-container">
-//                     <Row xs={1} md={2} className="g-4">
-//                         {Array.from({ length: 6 }).map((_, idx) => (
-//                             <Col>
-//                             <Project 
-//                             name={projectData.name}
-//                             info={projectData.info}
-//                             image={projectData.image}
-//                             github={projectData.github}
-//                             deployed={projectData.deployed}
-//                             />
-//                             </Col>
-//                         ))}
-//                     </Row>
-//                 </div>
-//             </Container>
-//         </div>
-//     )
-// }
 
 export default Projects
