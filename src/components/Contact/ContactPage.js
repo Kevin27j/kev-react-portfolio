3440c291c774f665b7827b38e1c24f7f70af6c5e
Kejvin Pashaj <kevin.952011@gmail.com>
2023-03-09 11:01:16 +0000

    responsive contact page

--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -13,7 +13,7 @@ function ContactPage() {
         <section>
             <h1 className="contact-title">Contact Me</h1>
             <Container>
-                <Row xs={1}>
+                <Row xs={1} md={1} lg={2}>
                     <Col className="contact-section">
                         <h3 className="contact-subtitle">Say Hi</h3>
                         <Form>
@@ -39,44 +39,53 @@ function ContactPage() {
                         <h3 className="contact-subtitle">Let's Connect</h3>
                         <ul className="contact-list">
                             <li>
-                                <a href="https://github.com/Kevin27j" target={"_blank"} className="contact-link">
+                                <div className="contact-element">
                                     <IconContext.Provider value={{ size: "2em" }}>
-                                        <div>
-                                            <FaGithub />
-                                        </div>
+                                        <FaGithub />
                                     </IconContext.Provider>
-                                    <h5>Github</h5>
-                                </a>
+                                    <a href="https://github.com/Kevin27j" target={"_blank"} className="contact-link">
+                                        <h4>Github</h4>
+                                    </a>
+                                </div>
                             </li>
+
                             <li>
-                                <a href="https://www.linkedin.com/in/kejvin-pashaj-672023218/" target={"_blank"} className="contact-link">
+                                <div className="contact-element">
                                     <IconContext.Provider value={{ size: "2em" }}>
                                         <div>
                                             <FaLinkedinIn />
                                         </div>
                                     </IconContext.Provider>
-                                    <h5>Linkedin</h5>
-                                </a>
+                                    <a href="https://www.linkedin.com/in/kejvin-pashaj-672023218/" target={"_blank"} className="contact-link">
+                                        <h4>Linkedin</h4>
+                                    </a>
+                                </div>
                             </li>
+
                             <li>
-                                <a href="mailto:kevin.952011@gmail.com" target={"_blank"} className="contact-link">
+                                <div className="contact-element">
                                     <IconContext.Provider value={{ size: "2em" }}>
                                         <div>
                                             <FaEnvelope />
                                         </div>
                                     </IconContext.Provider>
-                                    <h5>Email</h5>
-                                </a>
+                                    <a href="mailto:kevin.952011@gmail.com" target={"_blank"} className="contact-link">
+                                        <h4>Email</h4>
+                                    </a>
+                                </div>
                             </li>
+
                             <li>
-                                <a href="/" className="contact-link">
+                                <div className="contact-element">
                                     <IconContext.Provider value={{ size: "2em" }}>
                                         <div>
                                             <FaBriefcase />
                                         </div>
                                     </IconContext.Provider>
-                                    <h5>Resume</h5>
-                                </a>
+                                    <a href="/" className="contact-link">
+                                        <h4>Resume</h4>
+                                    </a>
+                                </div>
                             </li>
                         </ul>
                     </Col>
